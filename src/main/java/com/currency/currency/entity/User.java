c9481cf4debf903d53b1d5fa0e4aac4849da493b
MIkaLaY-YukhNik <uhnikkola03@gmail.com>
2025-06-09 15:26:41 +0300

    шиыв

--- src/main/java/com/currency/currency/entity/User.java
+++ src/main/java/com/currency/currency/entity/User.java
@@ -1,7 +1,7 @@
 package com.currency.currency.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-//import jakarta.persistence.*;
+
 
 import javax.persistence.*;
 import java.util.ArrayList;
