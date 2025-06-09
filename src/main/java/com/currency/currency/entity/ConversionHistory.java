1b35c916ab9559ac6ffb1aefed6e4c1483492708
MIkaLaY-YukhNik <uhnikkola03@gmail.com>
2025-06-09 15:26:04 +0300

    зщшне

--- src/main/java/com/currency/currency/entity/ConversionHistory.java
+++ src/main/java/com/currency/currency/entity/ConversionHistory.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-//import jakarta.persistence.*;
+
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
